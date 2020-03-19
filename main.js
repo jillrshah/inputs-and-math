235d1fd5718c3a2074dddf677fba86eecf029d45
Jill Shah <jillrshah@gmail.com>
2020-03-19 19:36:35 -0400

    Calculator updates

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 $("body").on("keyup keydown keypress change", ".item input", function (e) {
-	let total = 0;
+	let pre_tariff_total_sum = 0;
+	let post_tariff_total_sum = 0;
+
 	$("tr.item").each(function () {
 		const $this_row = $(this);
 		const quantity_field = $this_row.find(".qty");
@@ -16,8 +18,20 @@ $("body").on("keyup keydown keypress change", ".item input", function (e) {
 		const pre_tariff_total = qty * cost;
 
 		const post_tariff_total = (pre_tariff_total + (pre_tariff_total * pct));
+
+		pre_tariff_total_sum = pre_tariff_total_sum + pre_tariff_total;
+		post_tariff_total_sum = post_tariff_total_sum + post_tariff_total;
+
 		console.log(pre_tariff_total);
+		$this_row.find("td.pre_total span").text(pre_tariff_total);
+		$("#pre_total span").text(pre_tariff_total_sum);
+
 		console.log(post_tariff_total);
+		$this_row.find("td.post_total span").text(post_tariff_total);
+		$("#post_total span").text(post_tariff_total_sum);
+
+
+
 
 	});
 });
